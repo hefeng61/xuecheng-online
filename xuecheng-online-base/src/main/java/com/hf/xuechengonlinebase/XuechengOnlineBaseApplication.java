a2b12c2735f581c83dd5384935c3ed55c3272dcd
sodap <hofung61@gmail.com>
2024-02-20 09:26:32 +0800

    初始结构

---
+++ xuecheng-online-base/src/main/java/com/hf/xuechengonlinebase/XuechengOnlineBaseApplication.java
@@ -0,0 +1,13 @@
+package com.hf.xuechengonlinebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuechengOnlineBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuechengOnlineBaseApplication.class, args);
+    }
+
+}
---
+++ xuecheng-online-base/src/test/java/com/hf/xuechengonlinebase/XuechengOnlineBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hf.xuechengonlinebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuechengOnlineBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
